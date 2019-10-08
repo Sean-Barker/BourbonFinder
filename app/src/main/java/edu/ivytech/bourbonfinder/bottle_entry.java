4737de476f7f7388fc4f187b276447c9276dcc1b
Sean Barker <imseanbarker@gmail.com>
2019-10-07 21:10:09 -0400

    Initial commit

---
+++ app/src/main/java/edu/ivytech/bourbonfinder/bottle_entry.java
@@ -0,0 +1,14 @@
+package edu.ivytech.bourbonfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bottle_entry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottle_entry);
+    }
+}
---
+++ app/src/main/java/edu/ivytech/bourbonfinder/secondary_activity.java
@@ -0,0 +1,14 @@
+package edu.ivytech.bourbonfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class secondary_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary_activity);
+    }
+}
